a18718d1f06b6180bffd87433c288921e1da7ea4
mhakooma <mhakooma@nuevaclarity.com>
2022-07-26 12:08:37 +0200

    revisions

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,49 +1,46 @@
 #include "main.h"
 
 /**
- *read_textfile - reads a text file and
- *prints it to the POSIX standard output
- *@filename: file pointer
- *@letters: number of letters it should read and print
- *Return: actual number of letters it could read and print
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
+ *
+ * Return: the number of letters printed, or 0 if it failed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd;
-ssize_t fread, fwrite;
-char *buffer;
+	int fd;
+	int s, t;
+	char *buf;
 
-if (filename == NULL)
-return (0);
+	if (!filename)
+		return (0);
 
-/*open file*/
-fd = open(filename, O_RDONLY);
-if (fd == -1)
-return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
 
-/*malloc buffer*/
-buffer = (char *)malloc(sizeof(char) * letters);
-if (buffer == NULL)
-return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
 
-/*read file*/
-fread = read(fd, buffer, letters);
-if (fread == -1)
-{
-free(buffer);
-close(fread);
-return (0);
-}
+	s = read(fd, buf, letters);
+	if (s < 0)
+	{
+		free(buf);
+		return (0);
+	}
+	buf[s] = '\0';
 
-/*write file*/
-fwrite = write(STDOUT_FILENO, buffer, fread);
-if (fwrite == -1)
-{
-free(buffer);
-close(fd);
-return (0);
-}
+	close(fd);
 
-close(fd);
-return (fread);
+	t = write(STDOUT_FILENO, buf, s);
+	if (t < 0)
+	{
+		free(buf);
+		return (0);
+	}
+
+	free(buf);
+	return (t);
 }
